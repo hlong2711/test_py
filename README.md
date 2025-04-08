491cc19fc79986c8184563ee31ecec2fb7970bc1
Ha Long <longvn00@gmail.com>
2025-04-08 16:22:25 +0700

    feat: add some note

---
+++ README.md
@@ -0,0 +1,5 @@
+Use to learn basic python via notebook
+
+###
+- Install uv
+- Run `uv sync`
--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 def main():
     print("Hello from test!")
+    if 3 > 2:
+        print("five greater than two")
+
+        data = {
+            "a": 0,
+            "b": [
+                [1, 2],
+            ],
+        }
+
+        print("data", data, type(data))
+
+        val = ({"a": 1}, 1, 2)
+
+        a, *b, c = val
+        print("value: ", a, b, c)
+
+        a["a"] = 2
+        c = 3
+
+        print("after change ", a, val, sep=" | ")
+
+    x = 1  # int
+    y = 2.8  # float
+    z = 1j  # complex
+
+    print(type(x))
+    print(type(y))
+    print(type(z))
 
 
 if __name__ == "__main__":
